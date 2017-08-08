e3fe0ee48890d2acb065ee0d08f91edcbfcfb80c
ctb <bingo.chern@gmail.com>
2017-08-08 14:47:22 +0800

    字符串标准库

--- lua-5.1.4/src/lstrlib.c
+++ lua-5.1.4/src/lstrlib.c
@@ -59,6 +59,7 @@ static int str_reverse (lua_State *L) {
   luaL_Buffer b;
   const char *s = luaL_checklstring(L, 1, &l);
   luaL_buffinit(L, &b);
+  /* 颠倒字符串顺序 */
   while (l--) luaL_addchar(&b, s[l]);
   luaL_pushresult(&b);
   return 1;
