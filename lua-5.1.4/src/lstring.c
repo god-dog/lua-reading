ba490cec823eb3a807af522094d2101aa1fcadb1
ctb <bingo.chern@gmail.com>
2017-08-08 14:43:05 +0800

    字符串内部化

--- lua-5.1.4/src/lstring.c
+++ lua-5.1.4/src/lstring.c
@@ -29,18 +29,21 @@ void luaS_resize (lua_State *L, int newsize) {
   tb = &G(L)->strt;
   for (i=0; i<newsize; i++) newhash[i] = NULL;
   /* rehash */
+  /* 重新散列, 将旧hash位置所在的值移动到新hash位置 */
   for (i=0; i<tb->size; i++) {
     GCObject *p = tb->hash[i];
+    /* 遍历每个拉链 */
     while (p) {  /* for each node in the list */
       GCObject *next = p->gch.next;  /* save next */
       unsigned int h = gco2ts(p)->hash;
       int h1 = lmod(h, newsize);  /* new position */
       lua_assert(cast_int(h%newsize) == lmod(h, newsize));
-      p->gch.next = newhash[h1];  /* chain it */
+      p->gch.next = newhash[h1];  /* 前插至链首 *//* chain it */
       newhash[h1] = p;
       p = next;
     }
   }
+  /* 释放旧表内存 */
   luaM_freearray(L, tb->hash, tb->size, TString *);
   tb->size = newsize;
   tb->hash = newhash;
@@ -53,19 +56,24 @@ static TString *newlstr (lua_State *L, const char *str, size_t l,
   stringtable *tb;
   if (l+1 > (MAX_SIZET - sizeof(TString))/sizeof(char))
     luaM_toobig(L);
+  /* 分配内存并初始化 */
   ts = cast(TString *, luaM_malloc(L, (l+1)*sizeof(char)+sizeof(TString)));
   ts->tsv.len = l;
   ts->tsv.hash = h;
   ts->tsv.marked = luaC_white(G(L));
   ts->tsv.tt = LUA_TSTRING;
   ts->tsv.reserved = 0;
+  /* 将 `str` 复制至 `ts` 末尾*/
   memcpy(ts+1, str, l*sizeof(char));
-  ((char *)(ts+1))[l] = '\0';  /* ending 0 */
+  ((char *)(ts+1))[l] = '\0'; /* 字符串部分位于 `ts + 1 ~ ts +l`, 以'\0'结尾 /* ending 0 */
   tb = &G(L)->strt;
+  /* 计算位置, 对号入座, 前插至拉链的开头. 桶所在位置为字符串散列值关于桶个数的模. */
   h = lmod(h, tb->size);
   ts->tsv.next = tb->hash[h];  /* chain new entry */
   tb->hash[h] = obj2gco(ts);
+  /* 元素个数加一 */
   tb->nuse++;
+  /* 元素个数超过`size`, 全局字符串表 `strt` 过于稠密则扩容为原大小的2倍 */
   if (tb->nuse > cast(lu_int32, tb->size) && tb->size <= MAX_INT/2)
     luaS_resize(L, tb->size*2);  /* too crowded */
   return ts;
@@ -77,8 +85,12 @@ TString *luaS_newlstr (lua_State *L, const char *str, size_t l) {
   unsigned int h = cast(unsigned int, l);  /* seed */
   size_t step = (l>>5)+1;  /* if string is too long, don't hash all its chars */
   size_t l1;
+  /* JS Hash Function */
+  /* 计算JS Hash值 */
   for (l1=l; l1>=step; l1-=step)  /* compute hash */
     h = h ^ ((h<<5)+(h>>2)+cast(unsigned char, str[l1-1]));
+
+  /* 拉链法: 先定位到 `strt` 所在的桶, 然后遍历桶指向的链表, 比较其与 `str` 值相等的对象, 存在则返回 */
   for (o = G(L)->strt.hash[lmod(h, G(L)->strt.size)];
        o != NULL;
        o = o->gch.next) {
@@ -89,6 +101,7 @@ TString *luaS_newlstr (lua_State *L, const char *str, size_t l) {
       return ts;
     }
   }
+  /* 不存在创建一个字符串对象 */
   return newlstr(L, str, l, h);  /* not found */
 }
 
