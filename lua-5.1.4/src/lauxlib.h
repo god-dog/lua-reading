5abedb9b21500aba2421435c3deaeb56e3f4be75
ctb <bingo.chern@gmail.com>
2017-08-17 17:14:18 +0800

    lua库依赖的辅助函数

--- lua-5.1.4/src/lauxlib.h
+++ lua-5.1.4/src/lauxlib.h
@@ -109,6 +109,11 @@ LUALIB_API const char *(luaL_findtable) (lua_State *L, int idx,
 #define luaL_typename(L,i)	lua_typename(L, lua_type(L,(i)))
 
 /* lua主函数逻辑, `luaL_loadfile`对lua代码进行词法分析, 语法分析和代码生成, `lua_pcall`通过虚拟机中执行opcode */
+/*
++-------------+                   +--------------+
+|luaY_parser()|-----> Proto ----->|luaV_execute()|
++-------------+                   +--------------+
+ */
 #define luaL_dofile(L, fn) \
 	(luaL_loadfile(L, fn) || lua_pcall(L, 0, LUA_MULTRET, 0))
 
